77b8f24030f72da1ff1180387600d76232d972bc
Higuchi Osamu <toyukan@ring.box2.kmc.gr.jp>
2014-05-29 14:36:23 +0900

    add parser of AddExpression

--- TinyCParser.hs
+++ TinyCParser.hs
@@ -57,22 +57,49 @@ data CVal = Atom String
           | Integer Integer
 	  | Number Integer
 	  | Variation String
-	  | AddSubExpression {left :: CVal, op :: Char, right :: CVal}
+	  | Expression {left :: CVal, op :: Char, right :: CVal}
 
-idnetifier :: Parser String
-identifier = do
-    c <- char '_' <|> letter
-    cs <- many (try (alphaNum) <|> try(OneOf "_$"))
-    return (c : cs)
+--idnetifier :: Parser String
+--identifier = do
+--    c <- char '_' <|> letter
+--    cs <- many (try (alphaNum) <|> try(OneOf "_$"))
+--    return (c : cs)
 	  
 parseAdd :: Parser CVal
 parseAdd = do
+   x <- parseExpression
+   whiteSpace >> char '+' >> whiteSpace
+   y <- parseMul
+   return $ Expression x '+' y
+
+parseAddOrSub :: Parser CVal
+parseAddOrSub = do
+    whiteSpace
+    x <- try parseMul
+         <|> try parseAdd
+    return x
+
+parseMul :: Parser CVal
+parseMul = do
    x <- parseNumber
-   spaces >> char '+' >> spaces
-   y <- (try parseNumber)
-        <|> parseAdd
-   spaces >> char ';'
-   return $ AddSubExpression x '+' y
+   whiteSpace >> char '*' >> whiteSpace
+   y <- try parseMul
+        <|> parseNumber
+   return $ Expression x '*' y
+    
+parseExpression :: Parser CVal
+parseExpression = do
+    whiteSpace
+    exp <- parseAddOrSub
+    whiteSpace >> semi
+    return exp
+
+parseProgram :: Parser CVal
+parseProgram = do
+    whiteSpace
+    exp <- parseExpression
+    whiteSpace
+    return exp
 
 parseNumber :: Parser CVal
 parseNumber = liftM (Number . read) $ many1 digit
@@ -80,6 +107,6 @@ parseNumber = liftM (Number . read) $ many1 digit
 main :: IO ()
 main = do
    input <- getLine
-   print $ case parse parseAdd "TinyC" input of
+   print $ case parse parseProgram "TinyC" input of
       Left err -> "Error"
       Right val -> "Succeed"
